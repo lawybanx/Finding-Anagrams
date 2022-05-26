53fe91cf0df230d4d2b10cf142ae15520cd3db73
Awwal Bankole <awwalbankole@gmail.com>
2022-05-26 11:09:30 +0100

    completed task

---
+++ main.py
@@ -0,0 +1,17 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def check_anagram(word, anagram):
+    word = word.lower()
+    anagram = anagram.lower()
+    if len(word) != len(anagram) or sorted(word) != sorted(anagram):
+        return False
+    return True
+
+
+input_word = input("Enter Word: ")
+input_anagram = input("Enter anagram: ")
+print(check_anagram(input_word, input_anagram))
